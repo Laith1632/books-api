ddf96d420866610cac0b1ed55bd8cb515d66f365
Laith Albusairi <laithalbusairi@gmail.com>
2022-10-03 21:51:46 -0700

    got env crollers and modules

---
+++ server.js
@@ -0,0 +1,22 @@
+// DEPENDENCIES
+const express = require('express')
+const mongoose = require('mongoose')
+
+// CONFIGURATION
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+const app = express()
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, () => { console.log('connected to mongo: ', 
+process.env.MONGO_URI) }
+)
+
+// MIDDLEWARE
+app.use(express.json())
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
